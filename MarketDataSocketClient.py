b399ed7d32ccd1c11af55f65e28af82709ddce0f
jatin.kumawat <jatin.kumawat@rmoneyindia.com>
2024-10-30 12:09:15 +0530

    code refactor and requirement updation

--- MarketDataSocketClient.py
+++ MarketDataSocketClient.py
@@ -48,7 +48,7 @@ class MDSocket_io(socketio.Client):
         self.sid.on('connect', self.on_connect)
         self.sid.on('message', self.on_message)
 
-         """Similarly implement partial json full and binary json full."""
+        """Similarly implement partial json full and binary json full."""
         self.sid.on('1501-json-full', self.on_message1501_json_full)
         self.sid.on('1501-json-partial', self.on_message1501_json_partial)
 
@@ -134,11 +134,11 @@ class MDSocket_io(socketio.Client):
         """On receiving message code 1502 full"""
         print('I received a 1502 Market depth message!' + data)
 
-   def on_message1507_json_full(self, data):
+    def on_message1507_json_full(self, data):
         """On receiving message code 1507 full"""
         print('I received a 1507 MarketStatus message!' + data)
         
-   def on_message1512_json_full(self, data):
+    def on_message1512_json_full(self, data):
         """On receiving message code 1512 full"""
         print('I received a 1512 LTP message!' + data)     
 
